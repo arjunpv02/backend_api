cc865bf97a8c7c5f6339ed6c6eefb68e685b923f
ARJUN P V <125240263+arjunpv02@users.noreply.github.com>
2024-11-06 20:35:48 +0530

    12 latest commit

--- fealtyx/fealtyx/settings.py
+++ fealtyx/fealtyx/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-taa=*736nbhp&vti$6z1z5=vo&)34+jhhvi&!^g!vtb!zjc^#q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rest-auth.onrender.com']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
